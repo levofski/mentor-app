4854cc0b72149969aec96d46e7d931884b24584e
Matt McDonald <matt@fortybelow.ca>
2014-01-16 11:03:42 -0700

    COMPLETED:
        1. Added profile view.
    
        2. Updated style to include profile view.

---
+++ site/public/profile.php
@@ -0,0 +1,60 @@
+<!DOCTYPE HTML>
+<html dir="ltr" lang="en-US">
+	<head>
+		<meta http-equiv="Content-Type"
+			content="text/html;charset=utf-8">
+		<meta name="viewport"
+			content="initial-scale=1.0">
+		<title>PHP Mentoring: Profile&mdash;Null Industries</title>
+		<link href="https://fonts.googleapis.com/css?family=Architects+Daughter"
+			media="screen"
+			rel="stylesheet"
+			type="text/css">
+		<link href="./assets/styles/style-main.css"
+			media="all"
+			rel="stylesheet"
+			type="text/css">
+	</head>
+	<body>
+		<!-- {% include "./assets/templates/header.twig" %} -->
+		<div class="header_row primary_row row">
+			<div class="column header_column primary_column">
+				<div class="cell header_cell primary_cell">
+					<h1 class="header heading">PHP Mentoring</h1>
+					<div class="header_caption">Building strong developers</div>
+				</div>
+			</div>
+		</div>
+		<div class="primary_row row">
+			<div class="column primary_column">
+				<div class="cell primary_cell user_cell">
+					<div>/**</div>
+					<div>* @author <span class="user_name">Null Industries</span></div>
+					<div>* @email <span class="user_email">null@null.invalid</span></div>
+					<div>* @see <span class="user_github">github.com/nullindustries</span></div>
+					<div>* @see <span class="user_irc">nullindustries@freenode.net</span></div>
+					<div>* @see <span class="user_twitter">twitter.com/nullindustries</span></div>
+					<div>*/</div>
+				</div>
+				<div class="cell primary_cell user_cell">
+					<table class="table">
+						<thead class="table_section">
+							<tr class="table_row">
+								<th class="table_cell table_header">Mentor</th>
+								<th class="table_cell table_header">Apprentice</th>
+							</tr>
+						</thead>
+						<tbody class="table_section">
+							<tr class="table_row">
+								<td class="table_cell">&#x2713;</td>
+								<td class="table_cell">&#x2716;</td>
+							</tr>
+						</tbody>
+					</table>
+				</div>
+			</div>
+		</div>
+		<script src="./assets/js/backbone-min.js"
+			type="text/javascript"></script>
+	</body>
+</html>
