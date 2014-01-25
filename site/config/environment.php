3c1e1cd6c16904653f1375cbe4c13fe6d8b766cc
Matt Frost <mfrost.design@gmail.com>
2014-01-25 12:34:13 -0500

    Making some fixes to the Skill service to ensure the date was using/testing a DateTime object

--- site/config/environment.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-/**
- * @author  Stevan Goode <stevan@stevangoode.com>
- * @licence http://opensource.org/licences/MIT MIT
- */
-
-// Defines the environment for the system to use
-$config['environment'] = getenv('APP_ENV') ?: 'development';
