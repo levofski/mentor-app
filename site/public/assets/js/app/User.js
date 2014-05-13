9f002690454aac025b234a9810eded87ff5dacc0
kcrawley <kcmastrpc@gmail.com>
2014-05-13 10:23:19 -0500

    added new columns to User model and the ability to populate it based on the options.type property

--- site/public/assets/js/app/User.js
+++ site/public/assets/js/app/User.js
@@ -7,9 +7,20 @@ App.User = Backbone.Model.extend({
         email: '',
         irc_nick: '',
         github_handle: '',
+        github_uid: '',
         twitter_handle: '',
+        twitter_uid: '',
         skills: {},
         mentor_available: false,
-        apprentice_available: false,
+        apprentice_available: false
+    },
+    initialize: function(options) {
+        if (options.type == 'github') {
+            var profile = window.Mentor.user.externalProfile;
+
+            this.attributes.github_uid = profile.id,
+            this.attributes.github_handle = profile.name,
+            this.attributes.email = profile.email
+        }
     }
 });
