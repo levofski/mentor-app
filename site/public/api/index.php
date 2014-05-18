7617b30112a51390c6b087b7a5cbd1d4c3d88940
Matt Frost <mfrost.design@gmail.com>
2014-05-18 17:32:48 -0500

    Adding the exception catching to all the endpoints in the API

--- site/public/api/index.php
+++ site/public/api/index.php
@@ -241,136 +241,213 @@ $app->get('/v1/users', function() use ($app) {
 });
 
 $app->get('/v1/skills/:id', function($id) use ($app) {
-    $hashValidator = new \MentorApp\HashValidator();
-    if (!$hashValidator->validate($id)) {
-        $app->response->setStatus(404);
-        return;
+    try {
+        $hashValidator = new \MentorApp\HashValidator();
+        if (!$hashValidator->validate($id)) {
+            $app->response->setStatus(404);
+            return;
+        }
+        $skillService = new \MentorApp\SkillService($app->db);
+        $skillSerializer = new \MentorApp\SkillArraySerializer();
+        $skill = $skillService->retrieve($id);
+        $skillArray = $skillSerializer->toArray($skill);
+        if ($skill === null) {
+            $app->response->setStatus(404);
+            return;
+        }
+        $app->response->setStatus(200);
+        print json_encode($skillArray);    
+    } catch(\PDOException $e) {
+        $app->log->warn($e->getMessage() . ': ' . $e->getFile() . ':' . $e->getLine());
+        $app->setStatus(500);
+    } catch(\InvalidArgumentException $ie) {
+        $app->log->warn($ie->getMessage() . ': ' . $ie->getFile() . ':' . $ie->getLine());
+        $app->setStatus(500);
+    } catch(\RuntimeException $re) {
+        $app->log->warn($re->getMessage() . ': ' . $re->getFile() . ':' . $re->getLine());
+        $app->setStatus(500);
     }
-    $skillService = new \MentorApp\SkillService($app->db);
-    $skillSerializer = new \MentorApp\SkillArraySerializer();
-    $skill = $skillService->retrieve($id);
-    $skillArray = $skillSerializer->toArray($skill);
-    if ($skill === null) {
-        $app->response->setStatus(404);
-        return;
-    }
-    $app->response->setStatus(200);
-    print json_encode($skillArray);    
 });
 
 $app->delete('/v1/skills/:id', function($id) use ($app) {
-    $hashValidator = new \MentorApp\HashValidator();
-    if (!$hashValidator->validate($id)) {
-        $app->response->setStatus(404);
-        return;
+    try {
+        $hashValidator = new \MentorApp\HashValidator();
+        if (!$hashValidator->validate($id)) {
+            $app->response->setStatus(404);
+            return;
+        }
+        $skillService = new \MentorApp\SkillService($app->db);
+        if (!$skillService->delete($id)) {
+            $app->response->setStatus(404);
+            return;
+        }
+        $app->response->setStatus(200);
+    } catch(\PDOException $e) {
+        $app->log->warn($e->getMessage() . ': ' . $e->getFile() . ':' . $e->getLine());
+        $app->setStatus(500);
+    } catch(\InvalidArgumentException $ie) {
+        $app->log->warn($ie->getMessage() . ': ' . $ie->getFile() . ':' . $ie->getLine());
+        $app->setStatus(500);
+    } catch(\RuntimeException $re) {
+        $app->log->warn($re->getMessage() . ': ' . $re->getFile() . ':' . $re->getLine());
+        $app->setStatus(500);
     }
-    $skillService = new \MentorApp\SkillService($app->db);
-    if (!$skillService->delete($id)) {
-        $app->response->setStatus(404);
-        return;
-    }
-    $app->response->setStatus(200);
-});
+    });
 
 $app->post('/v1/skills', function() use ($app)  {
-    $skillService = new \MentorApp\SkillService($app->db);
-    $body = $app->request->getBody();
-    $skillArray = json_decode($body, true);
-    $skill = new \MentorApp\Skill();
-    ($skillArray['name'] !== null) ? $skill->name = htmlspecialchars($skillArray['name']) : $skill->name = null;
-    ($skillArray['added'] !== null) ? $skill->added = htmlspecialchars($skillArray['added']) : $skill->added = null;
-    ($skillArray['authorized'] !== null) ? $skill->authorized = htmlspecialchars($skillArray['authorized']) : $skill->authorized = null;
-    if (!$skillService->save($skill)) {
-        $app->response->setStatus(400);
-        return;
+    try {
+        $skillService = new \MentorApp\SkillService($app->db);
+        $body = $app->request->getBody();
+        $skillArray = json_decode($body, true);
+        $skill = new \MentorApp\Skill();
+        ($skillArray['name'] !== null) ? $skill->name = htmlspecialchars($skillArray['name']) : $skill->name = null;
+        ($skillArray['added'] !== null) ? $skill->added = htmlspecialchars($skillArray['added']) : $skill->added = null;
+        ($skillArray['authorized'] !== null) ? $skill->authorized = htmlspecialchars($skillArray['authorized']) : $skill->authorized = null;
+        if (!$skillService->save($skill)) {
+            $app->response->setStatus(400);
+            return;
+        }
+        $app->response->setStatus(201);
+    } catch(\PDOException $e) {
+        $app->log->warn($e->getMessage() . ': ' . $e->getFile() . ':' . $e->getLine());
+        $app->setStatus(500);
+    } catch(\InvalidArgumentException $ie) {
+        $app->log->warn($ie->getMessage() . ': ' . $ie->getFile() . ':' . $ie->getLine());
+        $app->setStatus(500);
+    } catch(\RuntimeException $re) {
+        $app->log->warn($re->getMessage() . ': ' . $re->getFile() . ':' . $re->getLine());
+        $app->setStatus(500);
     }
-    $app->response->setStatus(201);
 });
 
 $app->put('/v1/skills/:id', function($id) use ($app)   {
-    $hashValidator = new \MentorApp\HashValidator();
-    $skillService = new \MentorApp\SkillService($app->db);
-    $body = $app->request->getBody();
-    $skillArray = json_decode($body, true);
-    $skill = new \MentorApp\Skill();
-    ($id !== null) ? $skill->id = htmlspecialchars($id) : $skill->id = null;
-    ($skillArray['name'] !== null) ? $skill->name = htmlspecialchars($skillArray['name']) : $skill->name = null;
-    ($skillArray['added'] !== null) ? $skill->added = htmlspecialchars($skillArray['added']) : $skill->added = null;
-    ($skillArray['authorized'] !== null) ? $skill->authorized = htmlspecialchars($skillArray['authorized']) : $skill->authorized = null; 
-    if (!$hashValidator->validate($skill->id)) {
-        $app->response->setStatus(400);
-        return;
+    try { 
+        $hashValidator = new \MentorApp\HashValidator();
+        $skillService = new \MentorApp\SkillService($app->db);
+        $body = $app->request->getBody();
+        $skillArray = json_decode($body, true);
+        $skill = new \MentorApp\Skill();
+        ($id !== null) ? $skill->id = htmlspecialchars($id) : $skill->id = null;
+        ($skillArray['name'] !== null) ? $skill->name = htmlspecialchars($skillArray['name']) : $skill->name = null;
+        ($skillArray['added'] !== null) ? $skill->added = htmlspecialchars($skillArray['added']) : $skill->added = null;
+        ($skillArray['authorized'] !== null) ? $skill->authorized = htmlspecialchars($skillArray['authorized']) : $skill->authorized = null; 
+        if (!$hashValidator->validate($skill->id)) {
+            $app->response->setStatus(400);
+            return;
+        }
+        if (!$skillService->save($skill)) {
+            $app->response->setStatus(400);
+            return;
+        }
+        $app->response->setStatus(200);
+    } catch(\PDOException $e) {
+        $app->log->warn($e->getMessage() . ': ' . $e->getFile() . ':' . $e->getLine());
+        $app->setStatus(500);
+    } catch(\InvalidArgumentException $ie) {
+        $app->log->warn($ie->getMessage() . ': ' . $ie->getFile() . ':' . $ie->getLine());
+        $app->setStatus(500);
+    } catch(\RuntimeException $re) {
+        $app->log->warn($re->getMessage() . ': ' . $re->getFile() . ':' . $re->getLine());
+        $app->setStatus(500);
     }
-    if (!$skillService->save($skill)) {
-        $app->response->setStatus(400);
-        return;
-    }
-    $app->response->setStatus(200);
 });
 
 $app->get('/v1/partnerships/:id', function($id) use ($app) {
-    $partnershipManager = new \MentorApp\PartnershipManager($app->db);
-	$role = (!$app->request->get('role')) ? '' : $app->request->get('role');
-	$partnerships = $partnershipManager->retrieveByRole($role, $id);
+    try {
+        $partnershipManager = new \MentorApp\PartnershipManager($app->db);
+	    $role = (!$app->request->get('role')) ? '' : $app->request->get('role');
+	    $partnerships = $partnershipManager->retrieveByRole($role, $id);
 
-    if (empty($partnerships)) {
-        $app->response->setStatus(404);
-        return;
+        if (empty($partnerships)) {
+            $app->response->setStatus(404);
+            return;
+        }
+
+        $output = array();
+    
+        $userService = new \MentorApp\UserService($app->db);
+        $partnershipSerializer = new \MentorApp\PartnershipArraySerializer();
+        $userSerializer = new \MentorApp\UserArraySerializer();
+        $output = array();
+        foreach ($partnerships as $partnership) {
+            $mentor = $userService->retrieve($partnership->mentor);
+            $partnership->mentor = $userSerializer->toArray($mentor);
+            $apprentice = $userService->retrieve($partnership->apprentice);
+            $partnership->apprentice = $userSerializer->toArray($apprentice);
+            $output[] = $partnershipSerializer->toArray($partnership);
+        }
+
+        $app->response->setStatus(200);
+        print json_encode($output);
+    } catch(\PDOException $e) {
+        $app->log->warn($e->getMessage() . ': ' . $e->getFile() . ':' . $e->getLine());
+        $app->setStatus(500);
+    } catch(\InvalidArgumentException $ie) {
+        $app->log->warn($ie->getMessage() . ': ' . $ie->getFile() . ':' . $ie->getLine());
+        $app->setStatus(500);
+    } catch(\RuntimeException $re) {
+        $app->log->warn($re->getMessage() . ': ' . $re->getFile() . ':' . $re->getLine());
+        $app->setStatus(500);
     }
-
-    $output = array();
-
-    $userService = new \MentorApp\UserService($app->db);
-    $partnershipSerializer = new \MentorApp\PartnershipArraySerializer();
-    $userSerializer = new \MentorApp\UserArraySerializer();
-    $output = array();
-    foreach ($partnerships as $partnership) {
-        $mentor = $userService->retrieve($partnership->mentor);
-        $partnership->mentor = $userSerializer->toArray($mentor);
-        $apprentice = $userService->retrieve($partnership->apprentice);
-        $partnership->apprentice = $userSerializer->toArray($apprentice);
-        $output[] = $partnershipSerializer->toArray($partnership);
-    }
-
-    $app->response->setStatus(200);
-    print json_encode($output);
 });    
 
 $app->post('/v1/partnerships', function() use ($app) {
-    $requestData = $app->request->getBody();
-    $data = json_decode($requestData, true);
-    if (!isset($data['mentor']) || !isset($data['apprentice'])) {
+    try {
+        $requestData = $app->request->getBody();
+        $data = json_decode($requestData, true);
+        if (!isset($data['mentor']) || !isset($data['apprentice'])) {
+            $app->response->setStatus(400);
+            return;
+        }
+
+        $partnershipManager = new \MentorApp\PartnershipManager($app->db);
+        $userService = new \MentorApp\UserService($app->db);
+        $mentor = $userService->retrieve($data['mentor']);
+        $apprentice = $userService->retrieve($data['apprentice']);
+    
+        if ($partnershipManager->create($mentor, $apprentice)) {
+            $app->response->setStatus(201);
+            return;
+        }
+
         $app->response->setStatus(400);
-        return;
+    } catch(\PDOException $e) {
+        $app->log->warn($e->getMessage() . ': ' . $e->getFile() . ':' . $e->getLine());
+        $app->setStatus(500);
+    } catch(\InvalidArgumentException $ie) {
+        $app->log->warn($ie->getMessage() . ': ' . $ie->getFile() . ':' . $ie->getLine());
+        $app->setStatus(500);
+    } catch(\RuntimeException $re) {
+        $app->log->warn($re->getMessage() . ': ' . $re->getFile() . ':' . $re->getLine());
+        $app->setStatus(500);
     }
-
-    $partnershipManager = new \MentorApp\PartnershipManager($app->db);
-    $userService = new \MentorApp\UserService($app->db);
-    $mentor = $userService->retrieve($data['mentor']);
-    $apprentice = $userService->retrieve($data['apprentice']);
-
-    if ($partnershipManager->create($mentor, $apprentice)) {
-        $app->response->setStatus(201);
-        return;
-    }
-
-    $app->response->setStatus(400);
 });
 
 $app->delete('/v1/partnerships/:id', function($id) use ($app) {
-    $hashValidator = new \MentorApp\HashValidator();
-    if (!$hashValidator->validate($id)) {
-        $app->response->setStatus(404);
-        return;
-    }
+    try {
+        $hashValidator = new \MentorApp\HashValidator();
+        if (!$hashValidator->validate($id)) {
+            $app->response->setStatus(404);
+            return;
+        }
+    
+        $partnershipManager = new \MentorApp\PartnershipManager($app->db);
+        if ($partnershipManager->delete($id)) {
+            $app->response->setStatus(200);
+            return;
+        }
 
-    $partnershipManager = new \MentorApp\PartnershipManager($app->db);
-    if ($partnershipManager->delete($id)) {
-        $app->response->setStatus(200);
-        return;
+        $app->response->setStatus(400);
+    } catch(\PDOException $e) {
+        $app->log->warn($e->getMessage() . ': ' . $e->getFile() . ':' . $e->getLine());
+        $app->setStatus(500);
+    } catch(\InvalidArgumentException $ie) {
+        $app->log->warn($ie->getMessage() . ': ' . $ie->getFile() . ':' . $ie->getLine());
+        $app->setStatus(500);
+    } catch(\RuntimeException $re) {
+        $app->log->warn($re->getMessage() . ': ' . $re->getFile() . ':' . $re->getLine());
+        $app->setStatus(500);
     }
-
-    $app->response->setStatus(400);
 });
 
 $app->run();
